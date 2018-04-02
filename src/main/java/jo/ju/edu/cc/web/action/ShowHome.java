1d0feefec0b8185d05eb2791b6fd63b1752c03a0
amughrabi <ahmad.al_mughrabi@live.com>
2018-04-02 22:58:46 +0300

    MA assignment.

---
+++ src/main/java/jo/ju/edu/cc/web/action/ShowHome.java
@@ -0,0 +1,10 @@
+package jo.ju.edu.cc.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ShowHome extends ActionSupport {
+    @Override
+    public String execute() {
+        return SUCCESS;
+    }
+}
